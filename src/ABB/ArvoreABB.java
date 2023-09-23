770f05c0475b68a9bb99d3b498c8f42bc2d26bcc
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-22 23:41:16 -0300

    [add]: ABB (comentado)

--- src/ABB/ArvoreABB.java
+++ src/ABB/ArvoreABB.java
@@ -3,103 +3,103 @@ package ABB;
 import Nó.No;
 
 public class ArvoreABB<T extends Comparable<T>> {
-    private No<T> raiz;
+    private No<T> raiz; // Nó raiz da árvore
 
     public ArvoreABB() {
-        this.raiz = null;
+        this.raiz = null; // Inicializa a árvore com a raiz nula
     }
 
-    public void inserir(T elemento) {
-        this.raiz = inserirRecursivo(raiz, elemento);
+    public void inserir(T elemento) { // Insere um elemento na árvore
+        this.raiz = inserirRecursivo(raiz, elemento); // Chama o método recursivo de inserção
     }
 
-    private No<T> inserirRecursivo(No<T> no, T elemento) {
+    private No<T> inserirRecursivo(No<T> no, T elemento) { // Método recursivo de inserção
         if(no == null) {
-            return new No<>(elemento);
+            return new No<>(elemento); // Se o nó for nulo, cria um novo nó com o elemento
         }
 
         if (elemento.compareTo(no.getElemento()) < 0) {
-            no.setAnterior(inserirRecursivo(no.getAnterior(), elemento));
+            no.setAnterior(inserirRecursivo(no.getAnterior(), elemento)); // Se o elemento for menor que o elemento do nó, insere na subárvore esquerda
         } else if (elemento.compareTo(no.getElemento()) > 0) {
-            no.setProximo(inserirRecursivo(no.getProximo(), elemento));
+            no.setProximo(inserirRecursivo(no.getProximo(), elemento)); // Se o elemento for maior que o elemento do nó, insere na subárvore direita
         }
 
         return no;
     }
 
-    public boolean buscar(T elemento) {
-        return buscarRecursivo(raiz, elemento);
+    public boolean buscar(T elemento) { // Busca um elemento na árvore
+        return buscarRecursivo(raiz, elemento); // Chama o método recursivo de busca
     }
 
-    private boolean buscarRecursivo(No<T> no, T elemento) {
+    private boolean buscarRecursivo(No<T> no, T elemento) { // Método recursivo de busca
         if (no == null) {
-            return false;
+            return false; // Se o nó for nulo, retorna falso
         }
 
         if (elemento.compareTo(no.getElemento()) == 0) {
-            return true;
+            return true; // Se o elemento for igual ao elemento do nó, retorna verdadeiro
         }
 
         if (elemento.compareTo(no.getElemento()) < 0) {
-            return buscarRecursivo(no.getAnterior(), elemento);
+            return buscarRecursivo(no.getAnterior(), elemento); // Se o elemento for menor que o elemento do nó, busca na subárvore esquerda
         } else {
-            return buscarRecursivo(no.getProximo(), elemento);
+            return buscarRecursivo(no.getProximo(), elemento); // Se o elemento for maior que o elemento do nó, busca na subárvore direita
         }
     }
 
-    public void remover(T elemento) {
-        this.raiz = removerRecursivo(raiz, elemento);
+    public void remover(T elemento) { // Remove um elemento da árvore
+        this.raiz = removerRecursivo(raiz, elemento); // Chama o método recursivo de remoção
     }
 
     private No<T> removerRecursivo(No<T> no, T elemento) {
         if (no == null) {
-            return no;
+            return no; // Se o nó for nulo, retorna nulo
         }
 
         if (elemento.compareTo(no.getElemento()) < 0) {
-            no.setAnterior(removerRecursivo(no.getAnterior(), elemento));
+            no.setAnterior(removerRecursivo(no.getAnterior(), elemento)); // Se o elemento for menor que o elemento do nó, remove da subárvore esquerda
         } else if (elemento.compareTo(no.getElemento()) > 0) {
-            no.setProximo(removerRecursivo(no.getProximo(), elemento));
+            no.setProximo(removerRecursivo(no.getProximo(), elemento)); // Se o elemento for maior que o elemento do nó, remove da subárvore direita
         } else {
             if (no.getAnterior() == null) {
-                return no.getProximo();
+                return no.getProximo(); // Se o nó não tiver subárvore esquerda, retorna a subárvore direita
             } else if (no.getProximo() == null) {
-                return no.getAnterior();
+                return no.getAnterior(); // Se o nó não tiver subárvore direita, retorna a subárvore esquerda
             }
 
-            no.setElemento(encontrarMenorElemento(no.getProximo()));
+            no.setElemento(encontrarMenorElemento(no.getProximo())); // Se o nó tiver subárvore esquerda e direita, encontra o menor elemento da subárvore direita e substitui pelo elemento do nó
 
-            no.setProximo(removerRecursivo(no.getProximo(), no.getElemento()));
+            no.setProximo(removerRecursivo(no.getProximo(), no.getElemento())); // Remove o menor elemento da subárvore direita
         }
 
         return no;
     }
 
-    private T encontrarMenorElemento(No<T> no) {
+    private T encontrarMenorElemento(No<T> no) { // Encontra o menor elemento de uma subárvore
         if (no.getAnterior() == null) {
-            return no.getElemento();
+            return no.getElemento(); // Se o nó não tiver subárvore esquerda, retorna o elemento do nó
         }
 
-        return encontrarMenorElemento(no.getAnterior());
+        return encontrarMenorElemento(no.getAnterior()); // Se o nó tiver subárvore esquerda, busca na subárvore esquerda
     }
 
-    public void imprimir() {
-        imprimirRecursivo(raiz, 0);
+    public void imprimir() { // Imprime a árvore
+        imprimirRecursivo(raiz, 0); // Chama o método recursivo de impressão
     }
 
-    private void imprimirRecursivo(No<T> no, int nivel) {
+    private void imprimirRecursivo(No<T> no, int nivel) { // Método recursivo de impressão
         if (no == null) {
-            return;
+            return; // Se o nó for nulo, retorna
         }
 
-        imprimirRecursivo(no.getProximo(), nivel + 1);
+        imprimirRecursivo(no.getProximo(), nivel + 1); // Imprime a subárvore direita
 
-        for (int i = 0; i < nivel; i++) {
-            System.out.print("   "); // Ajuste o número de espaços conforme a profundidade desejada
+        for (int i = 0; i < nivel; i++) { // Imprime os espaços para ajustar a impressão
+            System.out.print("   ");
         }
 
-        System.out.println(no.getElemento());
+        System.out.println(no.getElemento()); // Imprime o elemento do nó
 
-        imprimirRecursivo(no.getAnterior(), nivel + 1);
+        imprimirRecursivo(no.getAnterior(), nivel + 1); // Imprime a subárvore esquerda
     }
 }
--- src/ABB/Teste.java
+++ src/ABB/Teste.java
@@ -2,9 +2,9 @@ package ABB;
 
 public class Teste {
     public static void main (String[] args) {
-        ArvoreABB<Integer> arvore = new ArvoreABB<>();
+        ArvoreABB<Integer> arvore = new ArvoreABB<>(); // Cria uma nova árvore binária de busca
 
-        arvore.inserir(10);
+        arvore.inserir(10); // Insere elementos na árvore
         arvore.inserir(5);
         arvore.inserir(15);
         arvore.inserir(3);
@@ -16,19 +16,19 @@ public class Teste {
         arvore.inserir(6);
         arvore.inserir(9);
 
-        int valorBuscado = 12;
-        boolean resultadoBusca = arvore.buscar(valorBuscado);
+        int valorBuscado = 12; // Busca um elemento na árvore
+        boolean resultadoBusca = arvore.buscar(valorBuscado); // Chama o método de busca
         if (resultadoBusca) {
-            System.out.println("O valor " + valorBuscado + " foi encontrado na árvore.");
+            System.out.println("O valor " + valorBuscado + " foi encontrado na árvore."); // Se o resultado da busca for verdadeiro, o elemento foi encontrado
         } else {
-            System.out.println("O valor " + valorBuscado + " não foi encontrado na árvore.");
+            System.out.println("O valor " + valorBuscado + " não foi encontrado na árvore."); // Se o resultado da busca for falso, o elemento não foi encontrado
         }
 
-//        int valorRemovido = 15;
-//        arvore.remover(valorRemovido);
-//        System.out.println("O valor " + valorRemovido + " foi removido da árvore.");
+        int valorRemovido = 15; // Remove um elemento da árvore
+        arvore.remover(valorRemovido); // Chama o método de remoção
+        System.out.println("O valor " + valorRemovido + " foi removido da árvore."); // Imprime o valor removido
 
-        System.out.println("Árvore binária de busca:");
-        arvore.imprimir();
+        System.out.println("Árvore binária de busca:"); // Imprime a árvore binária de busca
+        arvore.imprimir(); // Chama o método de impressão
     }
 }
